d60ae4cddaa880761696637274ddb02142e04c19
ethangnibus <ethangnibus@berkeley.edu>
2024-03-08 12:33:50 -0800

    working on making spancube work properly

--- src/components/SpanCube.tsx
+++ src/components/SpanCube.tsx
@@ -49,51 +49,70 @@ const SpanPlane: React.FC<SpanPlaneProps> = ({
     React.useEffect(() => {
         const mesh = meshRef.current;
 
-        // set the transform matrix for each instance
-        for (let width = 0; width <= planeWidth; width += 1) {
-            for (let height = 0; height <= planeWidth; height += 1) {
-                for (let depth = 0; depth <= planeWidth; depth += 1) {
-                    const offset_towards_a = spherePosition.clone().add(vector_a.clone().multiplyScalar(width - planeWidth/2));
-                    const offset_towards_b = spherePosition.clone().add(vector_b.clone().multiplyScalar(height - planeWidth/2));
-                    const offset_towards_c = spherePosition.clone().add(vector_c.clone().multiplyScalar(depth - planeWidth/2));
+        // width <=> a
+        // height <=> b
+        // depth <=> c
 
-                    // // planes orthoganal to a
-                    // scratchObject3D.position.copy(offset_towards_a);
-                    // scratchObject3D.rotation.copy(parallel_with_vector_b);
-                    // scratchObject3D.updateMatrix();
-                    // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
-
-                    // scratchObject3D.position.copy(offset_towards_a);
-                    // scratchObject3D.rotation.copy(parallel_with_vector_c);
-                    // scratchObject3D.updateMatrix();
-                    // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
-
-                    // // planes orthoganal to b
-                    // scratchObject3D.position.copy(offset_towards_b);
-                    // scratchObject3D.rotation.copy(parallel_with_vector_a);
-                    // scratchObject3D.updateMatrix();
-                    // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
-
-                    // scratchObject3D.position.copy(offset_towards_b);
-                    // scratchObject3D.rotation.copy(parallel_with_vector_c);
-                    // scratchObject3D.updateMatrix();
-                    // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
-
-                    // // planes orthoganal to c
-                    // scratchObject3D.position.copy(offset_towards_c);
-                    // scratchObject3D.rotation.copy(parallel_with_vector_a);
-                    // scratchObject3D.updateMatrix();
-                    // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
-
-                    scratchObject3D.position.copy(offset_towards_c);
-                    scratchObject3D.rotation.copy(parallel_with_vector_b);
-                    scratchObject3D.updateMatrix();
-                    if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
-
-
-                }
+        // vertical lines
+        for (let width = 0; width < planeWidth; width += 1) {
+            for (let depth = 0; depth < planeWidth; depth += 1) {
+                const offset_towards_a = spherePosition.clone().add(vector_a.clone().multiplyScalar(width - planeWidth/2));
+                const offset_towards_c = spherePosition.clone().add(vector_c.clone().multiplyScalar(depth - planeWidth/2));
+                
+                scratchObject3D.position.copy(offset_towards_a);
+                scratchObject3D.rotation.copy(parallel_with_vector_b);
+                scratchObject3D.updateMatrix();
+                if (mesh) mesh.setMatrixAt((width*planeWidth + depth), scratchObject3D.matrix);
             }
         }
+
+
+
+        // // set the transform matrix for each instance
+        // for (let width = 0; width <= planeWidth; width += 1) {
+        //     for (let height = 0; height <= planeWidth; height += 1) {
+        //         for (let depth = 0; depth <= planeWidth; depth += 1) {
+        //             const offset_towards_a = spherePosition.clone().add(vector_a.clone().multiplyScalar(width - planeWidth/2));
+        //             const offset_towards_b = spherePosition.clone().add(vector_b.clone().multiplyScalar(height - planeWidth/2));
+        //             const offset_towards_c = spherePosition.clone().add(vector_c.clone().multiplyScalar(depth - planeWidth/2));
+
+        //             // // planes orthoganal to a
+        //             // scratchObject3D.position.copy(offset_towards_a);
+        //             // scratchObject3D.rotation.copy(parallel_with_vector_b);
+        //             // scratchObject3D.updateMatrix();
+        //             // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
+
+        //             // scratchObject3D.position.copy(offset_towards_a);
+        //             // scratchObject3D.rotation.copy(parallel_with_vector_c);
+        //             // scratchObject3D.updateMatrix();
+        //             // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
+
+        //             // // planes orthoganal to b
+        //             // scratchObject3D.position.copy(offset_towards_b);
+        //             // scratchObject3D.rotation.copy(parallel_with_vector_a);
+        //             // scratchObject3D.updateMatrix();
+        //             // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
+
+        //             // scratchObject3D.position.copy(offset_towards_b);
+        //             // scratchObject3D.rotation.copy(parallel_with_vector_c);
+        //             // scratchObject3D.updateMatrix();
+        //             // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
+
+        //             // // planes orthoganal to c
+        //             // scratchObject3D.position.copy(offset_towards_c);
+        //             // scratchObject3D.rotation.copy(parallel_with_vector_a);
+        //             // scratchObject3D.updateMatrix();
+        //             // if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
+
+        //             scratchObject3D.position.copy(offset_towards_c);
+        //             scratchObject3D.rotation.copy(parallel_with_vector_b);
+        //             scratchObject3D.updateMatrix();
+        //             if (mesh) mesh.setMatrixAt((width*planeWidth*planeWidth + depth*planeWidth*planeWidth + height), scratchObject3D.matrix);
+
+
+        //         }
+        //     }
+        // }
         
         
         if (mesh) mesh.instanceMatrix.needsUpdate = true;
