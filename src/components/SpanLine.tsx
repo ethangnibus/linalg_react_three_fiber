dd56a5f0f30b0fa3f8e659c6f4e92172549b4787
ethangnibus <ethangnibus@berkeley.edu>
2024-03-11 19:34:10 -0700

    animated span line

--- src/components/SpanLine.tsx
+++ src/components/SpanLine.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { Cylinder } from '@react-three/drei';
 import * as THREE from 'three';
+import { useSpring, animated } from '@react-spring/three'
 
 interface SpanLineProps {
     spherePosition: THREE.Vector3;
@@ -32,19 +33,44 @@ const SpanLine: React.FC<SpanLineProps> = ({
         )
     );
 
+
+    const { scale: spanScale } = useSpring({
+        from: { scale: 0.0 }, // Initial scale is 0
+        to: { scale: 1.0 }, // Final scale is 1
+        config: { duration: 50000 }, // Animation duration in milliseconds
+      });
+
     return (
-        <Cylinder
-            position={spherePosition.toArray()}
-            rotation={rotationAngles}
-            args={[
-                0.01, // radiusTop
-                0.01, // radiusBottom
-                cylinderHeight,
-                8, // radialSegments
-            ]}
-            material={span_line_material}
-            renderOrder={0}
+        <animated.mesh
+        position={spherePosition.toArray()}
+        rotation={rotationAngles}
+        scale-x={1.0}
+        scale-y={spanScale}
+        scale-z={1.0}
+        renderOrder={0}
+    >
+        <cylinderGeometry args={[0.01, 0.01, cylinderHeight, 8]}/>
+        <meshToonMaterial
+            color={color}
+            transparent={true}
+            opacity={0.5}
+            blending={THREE.NormalBlending}
+            side={THREE.DoubleSide}
         />
+        
+    </animated.mesh>
+        // <Cylinder
+        //     position={spherePosition.toArray()}
+        //     rotation={rotationAngles}
+        //     args={[
+        //         0.01, // radiusTop
+        //         0.01, // radiusBottom
+        //         cylinderHeight,
+        //         8, // radialSegments
+        //     ]}
+        //     material={span_line_material}
+        //     renderOrder={0}
+        // />
     );
 };
 
