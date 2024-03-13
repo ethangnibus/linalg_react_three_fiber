4757db3e9d595a13f24bdce7b6582631da7565e5
ethangnibus <ethangnibus@berkeley.edu>
2024-03-13 00:27:48 -0700

    made vectorsphere div have drop shadow

--- src/App.tsx
+++ src/App.tsx
@@ -26,13 +26,16 @@ function App() {
     <MathJaxContext>
       <div className="w-full h-full fixed bg-white" onMouseMove={handleMouseMove}>
         {vectorSphereIsHovered && ( // Conditional rendering of infoBlock
-          <div className="z-40 w-32 h-32 absolute justify-center align-middle rounded-lg border-2 border-black bg-white" style={{ left: infoBlockPosition.x, top: infoBlockPosition.y }}>
-            {/* <div className="z-30 w-full h-full bg-white opacity-50 rounded-lg absolute"></div> */}
+        <>
+          <div className="z-10 w-32 h-32 absolute justify-center align-middle rounded-lg bg-black opacity-50 blur-md" style={{ left: infoBlockPosition.x, top: infoBlockPosition.y }}>
+            
+          </div>
+          <div className="z-20 w-32 h-32 absolute bg-white rounded-lg opacity-100" style={{ left: infoBlockPosition.x, top: infoBlockPosition.y }}></div>
+          <div className="z-30 w-32 h-32 absolute justify-center align-middle rounded-lg border-2 border-black" style={{ left: infoBlockPosition.x, top: infoBlockPosition.y }}>
             <p className="text-center select-none">Sphere Position</p>
             <MathJax className="z-20">{`\\begin{bmatrix} ${vectorSpherePosition.x.toFixed(3)} \\\\ ${vectorSpherePosition.y.toFixed(3)} \\\\ ${vectorSpherePosition.z.toFixed(3)} \\end{bmatrix}`}</MathJax>
-
           </div>
-        )}
+        </>)}
         <Canvas camera={{
           fov: 90,
           position: [2.5, 2, 1.5]
